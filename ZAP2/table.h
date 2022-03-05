09d14c5a626ac29c51e1a7bfdff0e855e57fbbed
aranjello <agoodman1996@gmail.com>
2022-03-05 14:43:58 -0700

    test git

--- ZAP2/table.h
+++ ZAP2/table.h
@@ -27,7 +27,7 @@ bool tableGet(Table* table, Key* key, Array* value);
 bool tableSet(Table* table, Key* key, Array* value);
 bool tableDelete(Table* table, ObjString* key);
 void tableAddAll(Table* from, Table* to);
-ObjString* tableFindString(Table* table, const char* chars,
+Key* tableFindString(Table* table, const char* chars,
                            int length, uint32_t hash);
 
 #endif
