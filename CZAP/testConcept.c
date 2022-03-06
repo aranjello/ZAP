b3a152593e22b52c0fddcd47f9619e825e7a1cde
Andrew Goodman <agoodman1996@gmail.com>
2022-03-06 13:37:38 -0700

    removed old files

--- CZAP/testConcept.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-int maint(int argc, const char* argv[]) {
-    char str[10];
-    strcpy(str, "str");
-    printf("%s", strcmp(strcat(str,"ing"),"string")==0 ? "t":"f");
-}
